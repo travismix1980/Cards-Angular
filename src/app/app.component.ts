22490d84a8a04fe781af7d7328141801802934cc
Travis Mix <travismix1980@gmail.com>
2021-02-14 18:05:59 -0800

    data attached and sent from app-component to app-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/images/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this awesome tree during my hike today',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/images/mountain.jpeg',
+      username: 'mountainLover',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/images/biking.jpeg',
+      username: 'biking1234',
+      content: 'I did some biking today',
+    },
+  ];
 }
